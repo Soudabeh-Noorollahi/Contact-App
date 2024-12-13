f191d9effcb70d0793c2226e9d63730e4bd71bbe
Soudabeh Noorollahi <s.nourollahii@gmail.com>
2024-12-13 22:38:50 +0100

    Initial commit

---
+++ src/constants/inputs.js
@@ -0,0 +1,7 @@
+const inputs = [
+  { type: "text", name: "name", placeholder: "Name..." },
+  { type: "text", name: "lastName", placeholder: "Las Name..." },
+  { type: "email", name: "email", placeholder: "Email ..." },
+  { type: "number", name: "phone", placeholder: "Phpne..." },
+];
+export default inputs;
